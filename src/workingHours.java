4476040a0598f6d5087e6971905e41b5dd37f27c
baechaeyoon <baechaeyoon@naver.com>
2023-02-02 20:04:57 +0900

    근무시간

---
+++ src/workingHours.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+public class workingHours {
+    public static void main(String args[])
+    {
+        try{
+
+            int answer = 0;
+
+            Scanner sc = new Scanner(System.in);
+
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+            for(int i =0; i<5; i++){
+
+                String start = sc.next();
+                String end = sc.next();
+
+                Date startTime = sdf.parse(start);
+                Date endTime = sdf.parse(end);
+
+                long timeStart = startTime.getTime();
+                long timeEnd = endTime.getTime();
+
+                long diff = timeEnd - timeStart;
+
+                long diffMin = diff  /(1000 * 60);
+
+                answer += diffMin;
+            }
+            System.out.println(answer);
+        }
+        catch(ParseException e){
+            e.printStackTrace();
+        }
+
+    }
+}

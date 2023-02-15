e61ff28b15cd3435b6286fde0c3508e4b806d417
baechaeyoon <baechaeyoon@naver.com>
2023-02-16 00:12:13 +0900

    8단 변속기

---
+++ src/speedGearbox.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 8단변속기
+ * level 2
+ * */
+
+public class speedGearbox {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[8];
+        String answer = null;
+
+        try{
+
+            for(int i = 0; i<arr.length; i++){
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            for(int i =0; i<arr.length-1; i++){
+                if(arr[i+1] == arr[i]+1){
+                    answer = "ascending";
+                }
+                else if(arr[i+1] == arr[i]-1){
+                    answer = "descending";
+                }
+                else{
+                    answer = "mixed";
+                    break;
+                }
+            }
+             System.out.println(answer);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}

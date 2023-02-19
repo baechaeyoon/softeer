1c08da42886cff752ff3102fe795aa7c4707033e
baechaeyoon <baechaeyoon@naver.com>
2023-02-19 22:12:09 +0900

    A+B

--- src/aPlusB.java
+++ src/aPlusB.java
@@ -10,8 +10,8 @@ public class aPlusB
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-
         int N = sc.nextInt();
+
         for(int i =0; i<N; i++){
 
             int A = sc.nextInt();
